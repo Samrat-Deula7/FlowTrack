b05e94824f8f3d199484337e63f62550c4940faa
Samrat-Deula7 <samratdeula461@gmail.com>
2026-01-29 21:37:49 +0545

    Working on the footer

---
+++ Frontend/src/LandingPageComponents/Footer.tsx
@@ -0,0 +1,75 @@
+import Logo from "../assets/logo.png";
+import Github from "../assets/github.png"
+import Linkdin from "../assets/linkedin.png"
+import FB from "../assets/facebook.png"
+import Insta from "../assets/instagram.png"
+
+const Footer = () => {
+  return (
+    <footer className="bg-gradient-to-br from-black to-slate-950 text-gray-400 py-8 px-6 flex justify-center items-center">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-6 mb-6">
+          <div className="flex justify-center items-center gap-2 text-xl ">
+            <img src={Logo} alt="logo" className="w-8" />
+            <h1 className="text-white font-bold">
+              <div className="flex">
+                Flow <span className="text-green-500">Track</span>
+              </div>
+            </h1>
+          </div>
+
+          <div className="flex gap-8 text-sm">
+            <a href="#" className="hover:text-white transition">
+              About
+            </a>
+            <a href="#" className="hover:text-white transition">
+              Privacy
+            </a>
+            <a href="#" className="hover:text-white transition">
+              Terms
+            </a>
+            <a href="#" className="hover:text-white transition">
+              Contact
+            </a>
+          </div>
+
+          <div className="flex gap-4">
+            <a
+              href="https://github.com/Samrat-Deula7"
+              className="hover:text-white transition"
+            >
+              <img src={Github} alt="" className="w-6 bg-white rounded-full" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/samrat-deula-412531369/"
+              className="hover:text-white transition"
+            >
+              <img src={Linkdin} alt="" className="w-6" />
+            </a>
+            <a
+              href="https://www.facebook.com/samrat.deula.52"
+              className="hover:text-white transition "
+            >
+              <img src={FB} alt="" className="w-6" />
+            </a>
+            <a
+              href="https://www.instagram.com/deula.samrat/"
+              className="hover:text-white transition"
+            >
+              <img src={Insta} alt="" className="w-6" />
+            </a>
+          </div>
+        </div>
+
+        <div className="border-t border-gray-800 pt-6 text-center text-sm">
+          <p>
+            © 2026 Flow<span className="text-green-500">Track</span>. All rights
+            reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
