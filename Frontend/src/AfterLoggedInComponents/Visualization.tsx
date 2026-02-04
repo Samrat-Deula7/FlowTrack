cd94c90532e6ab686f59f2cff8f6834c1856fbe4
Samrat-Deula7 <samratdeula461@gmail.com>
2026-02-04 07:21:51 +0545

    Worked on the vusualization scafold UI

--- Frontend/src/AfterLoggedInComponents/Visualization.tsx
+++ Frontend/src/AfterLoggedInComponents/Visualization.tsx
@@ -3,8 +3,31 @@ import Nav from "./Nav";
 export default function Visualization() {
   return (
     <>
-    <Nav/>
-      <h1 className="text-white">This is Visualization</h1>
+      <Nav />
+      <div className="w-full min-h-screen px-4 sm:px-6 md:px-8 lg:px-10 2xl:px-12 py-6 sm:py-8 md:py-10 lg:py-12">
+        <div className="flex flex-col  justify-center items-center lg:items-start gap-4 sm:gap-5 md:gap-6 lg:gap-8 w-full mx-auto">
+          <div className="flex flex-col 2xl:flex-row gap-4 sm:gap-5 md:gap-6  lg:flex-1 w-full">
+            <div
+              className="w-full 
+                          h-48 sm:h-56 md:h-80 lg:h-96 2xl:h-190
+                          bg-white/20 backdrop-blur-md shadow-lg rounded-2xl border border-white/10"
+            ></div>
+
+            <div
+              className="w-full 
+                          h-40 sm:h-48 md:h-56 lg:h-64 2xl:h-190 
+                          bg-white/20 backdrop-blur-md shadow-lg rounded-2xl border border-white/10"
+            ></div>
+          </div>
+
+          
+            <div
+              className="w-full 
+                          h-48 sm:h-56 md:h-80 lg:h-96 2xl:h-190
+                          bg-white/20 backdrop-blur-md shadow-lg rounded-2xl border border-white/10"
+            ></div>
+        </div>
+      </div>
     </>
   );
 }
