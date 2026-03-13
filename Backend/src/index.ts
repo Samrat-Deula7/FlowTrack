6c8b136415f23dffa83f1a711945189aa2c4d974
Samrat-Deula7 <samratdeula461@gmail.com>
2026-03-13 10:07:32 +0545

    Working on the backend of the flow track project

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -7,6 +7,8 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server!");
 });
 
+// app.use("api/tasks",require("./routes/Task.ts"))
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
