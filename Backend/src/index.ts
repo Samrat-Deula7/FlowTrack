340c47421b76bb63b6fded2f84f109f778dd1f0b
Samrat-Deula7 <samratdeula461@gmail.com>
2026-03-13 11:20:03 +0545

    started work on the api

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,10 @@
 import express, { Application, Request, Response } from "express";
+import tasksRouter from "./Task";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server!");
-});
-
-// app.use("api/tasks",require("./routes/Task.ts"))
+app.use("/api/tasks", tasksRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
