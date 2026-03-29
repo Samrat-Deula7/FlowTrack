9501cf7ea72a363cf49ebc4ace7e2a6b2f4b364a
Samrat-Deula7 <samratdeula461@gmail.com>
2026-03-29 17:01:29 +0545

    Working on connecting with mysql database

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,14 @@
 import express, { Application, Request, Response } from "express";
 import tasksRouter from "./routes/Task";
+import cors from "cors";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
+app.use(express.json())
+
+app.use(cors())
 app.use("/api/tasks", tasksRouter);
 
 app.listen(PORT, () => {
--- Backend/src/routes/Task.ts
+++ Backend/src/routes/Task.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import sql from "mssql";
+
 const router = express.Router();
 
 router.get("/createtasks",(req: Request, res: Response) => {
-    res.send("haslddshf");
+    res.send("Hi this the backend for task api");
   });
 
 export default router;
