becce53db0f2d8326bb7feb1f4742fcb6d7661a1
Samrat-Deula7 <samratdeula461@gmail.com>
2026-03-29 18:26:31 +0545

    Connected to mysql database successfully

--- Backend/src/routes/Task.ts
+++ Backend/src/routes/Task.ts
@@ -3,9 +3,29 @@ import sql from "mssql";
 
 const router = express.Router();
 
-router.get("/createtasks",(req: Request, res: Response) => {
-    res.send("Hi this the backend for task api");
-  });
+const config = {
+  user: "Flow_Track",
+  password: "1111",
+  server: "DESKTOP-DVSO359",
+  database: "Project_Flow_Track",
+  options: {
+    trustServerCertificate: true,
+    trustedConnection: false,
+    enableArithAbort: true,
+    instancename: "SQLEXPRESS",
+  },
+  port: 1433,
+};
+
+router.get("/createtasks", async (req: Request, res: Response) => {
+  try {
+    const pool = await sql.connect(config);
+    const data = await pool.request().query("SELECT * FROM tasks");
+    return res.json(data.recordset); // ✅ return the rows array
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Some error occurred");
+  }
+});
 
 export default router;
-
