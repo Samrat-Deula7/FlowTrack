66ddf224f3790f6b41fdb3b3713f48832690026b
Samrat-Deula7 <samratdeula461@gmail.com>
2026-03-30 08:52:23 +0545

    Working on the login system

--- Backend/src/routes/Task.ts
+++ Backend/src/routes/Task.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from "express";
+import {body,validationResult} from "express-validator"
+import bcrypt from "bcryptjs"
 import dotenv from "dotenv";
 import sql from "mssql";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// import jwt from "jsonwebtoken"
 
 const router = express.Router();
 dotenv.config();
@@ -17,6 +23,28 @@ const config = {
   },
   port: 1433,
 };
+
+router.post(
+  "/loginUser",
+  [
+    body("name", "Name should have at least 3 characters").isLength({ min: 3 }),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+    body("email", "Enter a valid email").isEmail(),
+    body("phoneno", "Phoneno must be at least have 10 numbers").isLength({
+      min: 10,
+    })
+  ],
+  async (req: Request, res: Response) => {
+    // The above array will set the restrictions rules and the following code will give error if those rules are broken.
+    const errors = validationResult(req);
+    // If error is empty is false then there is error so the if statement cathes the error.
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 router.get("/GetAllTasks", async (req: Request, res: Response) => {
   try {
